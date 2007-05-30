64dbe60448c3277a813fd763bef5778a228e424e
debogdano <debogdano@f17b339f-9c22-0410-8116-9f36aa46d225>
2007-05-30 05:29:54 +0000

    Added rellink.py, one stupid function to create relative symlink paths.

---
+++ cookbook/rellink.py
@@ -0,0 +1,31 @@
+"""
+Finds the common parent directory in order to have the closest relative
+symlinks. Useful when the links are going to be created by non-humans.
+
+Not using os.path.commonprefix just because it is character-based and I'm
+not in which cases it will be unapropriated.
+
+Bogdano Arendartchuk <debogdano@gmail.com>
+may/2007
+"""
+
+def rellink(src, dst):
+    """Creates relative symlinks
+
+    It will find the common ancestor and append to the src path.
+    """
+    asrc = os.path.abspath(src)
+    adst = os.path.abspath(dst)
+    csrc = asrc.split(os.path.sep)
+    cdst = adst.split(os.path.sep)
+    dstname = cdst.pop()
+    i = 0
+    l = min(len(csrc), len(cdst))
+    while i < l:
+        if csrc[i] != cdst[i]:
+            break
+        i += 1
+    dstextra = len(cdst[i:])
+    steps = [os.path.pardir] * dstextra
+    steps.extend(csrc[i:])
+    return os.path.sep.join(steps)
